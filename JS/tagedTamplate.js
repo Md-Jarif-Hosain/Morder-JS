c97536893dae6c9584c48115147a30ed5a76eb8b
Jarif Hosain <hosainjarif@gmail.com>
2022-05-24 01:10:31 +0600

    taged tamplate added

--- JS/tagedTamplate.js
+++ JS/tagedTamplate.js
@@ -1,10 +1,11 @@
 function modifier(strings, ...values) {
     const m = strings.reduce((prev, current) => {
-        return prev + current + (values.length ? "Mr " + values.shift() : "");
-    }, "");
-    return m
+        return prev + current + (values.length ? "Mr " + values.shift() : "")
+    }, "")
+    return m;
 }
 var player1 = "sakib";
 var player2 = "Jarif";
 
+// console.log(modifier`We have ${player1} and ${player2} in our team`);
 console.log(modifier`We have ${player1} and ${player2} in our team`);
